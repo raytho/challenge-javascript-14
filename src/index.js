4fe99fc2584b18b28a05298f73f1c51a270062eb
Raymundo Solis <pepperray01@gmail.com>
2020-09-09 01:01:42 -0500

    Se agrega solucion

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 function diamond(size) {
-  // your code
-  return;
-}
+  var draw = "";
+  if (size > 0 && size % 2 == 0 && !size) {
+    return `invalid number`;
+  } else {
+    for (let i = 0; i < size; i++) {
+      if (i % 2 === 0) {
+        for (let k = 0; k < size - i; k++) {
+          draw += "\xa0";
+        }
 
+        for (let j = 0; j < i + 1; j++) {
+          draw += "* ";
+        }
+        draw += " \n";
+      }
+    }
+
+    for (let i = size; i >= 0; i--) {
+      if (i % 2 === 0 || i === size) {
+        for (let k = 0; k < size - i; k++) {
+          draw += "\xa0";
+        }
+
+        for (let j = 0; j < i + 1; j++) {
+          draw += "* ";
+        }
+        draw += " \n";
+      }
+    }
+  }
+  return draw;
+}
+console.log(diamond(5));
 module.exports = { diamond };
